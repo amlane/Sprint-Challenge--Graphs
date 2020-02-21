f89f4b832966125627ce94dbf85d2f3d6913e4ba
amlane <twelvexstring@gmail.com>
2020-02-20 22:43:30 -0600

    TESTS PASSED: 23064 moves, 500 rooms visited

--- adv.py
+++ adv.py
@@ -11,9 +11,9 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
@@ -31,37 +31,88 @@ player = Player(world.starting_room)
 traversal_path = []
 
 
+def flip_dir(dir):
+    if dir == "n":
+        return "s"
+    if dir == "s":
+        return "n"
+    if dir == "w":
+        return "e"
+    if dir == "e":
+        return "w"
+    else:
+        return "error"
+
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
 def dft_walker():
-    # if player's current room is not in the dictionary add it, with an empty dict as it's value
     visited = {}
-    if player.current_room.id not in visited:
-        # then get available exits for room
-        # set each item in direction to a ? in visited for current room id
-        tempVar = {}
+    count = 0
+
+    # put the first room in the dictionary
+    visited[player.current_room.id] = {}
+    # while count < 100:
+    #     count += 1
+    while len(visited) < len(room_graph):
+        # Sets any directions not yet explored to "?" in visited
         for direction in player.current_room.get_exits():
-            tempVar[direction] = "?"
-        visited[player.current_room.id] = tempVar
+            if direction not in visited[player.current_room.id]:
+                # if it doesn't, add it with a value of "?"
+                visited[player.current_room.id][direction] = "?"
+                # if it does, don't do anything - done
 
-    # else, or after that...
-    # pick a random unexplored direction from the player's current room
-    exits = player.current_room.get_exits()
-    random.shuffle(exits)
-    dir = exits[-1]
-    # if the room is unexplored...
-    if visited[player.current_room.id][dir] == "?":
-        # remember the prev room id
-        prev_room_id = player.current_room.id
-        # travel to the new room and log the direction in traversal path
-        player.travel(dir)
-        traversal_path.append(dir)
-        # update the entry in visited ditionary
-        visited[prev_room_id][dir] = player.current_room.id
-    print(visited)
+        # if there are not unexplored rooms, do this...
+        if "?" not in visited[player.current_room.id].values():
+            # pick a random room until you find an unexplored room
+            exits = player.current_room.get_exits()
+            random.shuffle(exits)
+            dir = exits[-1]
+            prev_room_id = player.current_room.id
+            # travel to the new room
+            player.travel(dir)
+            # log the direction in traversal path
+            traversal_path.append(dir)
 
-    # loop until you reach a room with no unexplored paths
+        else:
+            # if there are unexplored rooms...
+            # picks an unexplored room and travels to it, updates the traversal path and updates visited
+            exits = player.current_room.get_exits()
+            random.shuffle(exits)
+            dir = exits[-1]
+            # if the room is unexplored...
+            if visited[player.current_room.id][dir] == "?":
+                # remember the prev room id
+                prev_room_id = player.current_room.id
+                # travel to the new room
+                player.travel(dir)
+                # log the direction in traversal path
+                traversal_path.append(dir)
+                # update the entry in visited ditionary
+                visited[prev_room_id][dir] = player.current_room.id
+                visited[player.current_room.id] = {}
+                visited[player.current_room.id][flip_dir(dir)] = prev_room_id
+        # print(visited)
 
 
 dft_walker()
+
+print(traversal_path)
 
 
 # TRAVERSAL TEST
